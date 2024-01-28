f998e81a23482d65fc80ea44f06a372e5a24ac1e
v8yte <buihachi.nishi@gmail.com>
2024-01-28 10:03:39 +0900

    add: checklines

--- js/model/gameModel.js
+++ js/model/gameModel.js
@@ -169,6 +169,34 @@ class GameModel {
     checkGameOver() {
         // ゲームオーバーの条件をチェックし、その結果を返す
     }
+
+    // 任意のラインを消去
+    removeLine(y) {
+        this.grid.splice(y, 1);
+        this.grid.unshift(Array(10).fill(0));
+    }
+
+    // 指定された行が完全に埋まっているかチェック
+    isLineComplete(y) {
+        for (let x = 0; x < this.grid[y].length; x++) {
+            if (this.grid[y][x] === 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // 埋まった行のチェック/消去した行数のカウント
+    checkLines() {
+        let lines = 0;
+        for (let y = 0; y < this.grid.length; y++) {
+            if (this.isLineComplete(y)) {
+                this.removeLine(y);
+                lines++;
+            }
+        }
+        return lines;
+    }
 }
 
 module.exports = GameModel;
