a61ec2bf7f842195907fc1ed3de18f047201ed8b
v8yte <buihachi.nishi@gmail.com>
2024-01-28 10:03:39 +0900

    add: checklines

--- __tests__/gameModel.test.js
+++ __tests__/gameModel.test.js
@@ -265,4 +265,61 @@ describe('GameModel', () => {
             expect(gameModel.checkGameOver()).toBeFalsy();
         });
     });
+
+    describe('removeLine', () => {
+        test('should remove line', () => {
+            const gameModel = new GameModel();
+            gameModel.grid = [
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+                [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+                [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+                [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+            ];
+
+            gameModel.removeLine(3);
+            expect(gameModel.grid).toEqual([
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+                [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+                [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+            ]);
+        });
+    });
+
+    describe('isLineComplete', () => {
+        test('should return true when line complete', () => {
+            const gameModel = new GameModel();
+            gameModel.grid[0] = [1, 2, 3, 4, 5, 6, 7, 1, 2, 3];
+            expect(gameModel.isLineComplete(0)).toBeTruthy();
+        });
+
+        test('should return false when line not complete', () => {
+            const gameModel = new GameModel();
+            gameModel.grid[0] = [1, 0, 3, 4, 5, 6, 7, 1, 2, 3];
+            expect(gameModel.isLineComplete(0)).toBeFalsy();
+        });
+    });
+
+    describe('checkLines', () => {
+        test('should remove line and return number of lines removed', () => {
+            const gameModel = new GameModel();
+            gameModel.grid = [
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 0], 
+                [2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
+                [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+                [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+            ];
+
+            const lines = gameModel.checkLines();
+            expect(lines).toBe(2);
+            expect(gameModel.grid).toEqual([
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 0], 
+                [2, 2, 2, 2, 2, 2, 2, 2, 2, 0]
+            ]);
+        });
+    });
 });
